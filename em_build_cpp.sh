304b4768165f34d6f3cc2f243139c2acb95694fe
Joni Rapo <jonirapo@gmail.com>
2025-10-19 17:34:23 +0300

    added HEAPF64 to exported runtime methods

--- em_build_cpp.sh
+++ em_build_cpp.sh
@@ -5,5 +5,5 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 mkdir -p $SCRIPT_DIR/em_bin
 
-em++ -fdeclspec -sMODULARIZE=1 -sEXPORTED_FUNCTIONS="['_malloc','_free','_EMSCRIPTEN_ITM_P2P_TLS_Ex_str']" -sEXPORTED_RUNTIME_METHODS='cwrap,ccall,setValue,getValue' -sENVIRONMENT=web -o $SCRIPT_DIR/em_bin/itm.js $(ls $SCRIPT_DIR/src/*.cpp)
+em++ -fdeclspec -sMODULARIZE=1 -sEXPORTED_FUNCTIONS="['_malloc','_free','_EMSCRIPTEN_ITM_P2P_TLS_Ex_str']" -sEXPORTED_RUNTIME_METHODS='cwrap,ccall,setValue,getValue,HEAPF64' -sENVIRONMENT=web -o $SCRIPT_DIR/em_bin/itm.js $(ls $SCRIPT_DIR/src/*.cpp)
 
